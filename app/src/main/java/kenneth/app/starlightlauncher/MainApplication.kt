89a9b48b02159f4ea08a55b4f51531d1fb2034f7
Kenneth <kenneth.ng.5226@outlook.com>
2022-02-06 22:31:00 +0000

    Add support for Material You color

--- app/src/main/java/kenneth/app/starlightlauncher/MainApplication.kt
+++ app/src/main/java/kenneth/app/starlightlauncher/MainApplication.kt
@@ -1,7 +1,13 @@
 package kenneth.app.starlightlauncher
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
