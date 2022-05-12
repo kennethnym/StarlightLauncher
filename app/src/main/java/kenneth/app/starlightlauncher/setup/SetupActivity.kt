a0f0ac3378e478f793efd0315aa459d8a74b199a
Kenneth <kenneth.ng.5226@outlook.com>
2022-05-12 16:45:52 +0100

    Add launcher icon

--- app/src/main/java/kenneth/app/starlightlauncher/setup/SetupActivity.kt
+++ app/src/main/java/kenneth/app/starlightlauncher/setup/SetupActivity.kt
@@ -3,6 +3,7 @@ package kenneth.app.starlightlauncher.setup
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import kenneth.app.starlightlauncher.R
 import kenneth.app.starlightlauncher.databinding.ActivitySetupBinding
 
 class SetupActivity : AppCompatActivity() {
@@ -27,7 +28,7 @@ class SetupActivity : AppCompatActivity() {
 
     private fun goToNextStep() {
         if (binding.setupPager.currentItem == SETUP_STEP_COUNT - 1) {
-            // finish setup
+            binding.continueBtn.text = getString(R.string.action_finish)
         } else {
             binding.setupPager.currentItem += 1
         }
